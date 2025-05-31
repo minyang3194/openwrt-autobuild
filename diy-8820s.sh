c5b62480c8a5bb50e20fd996e9a9b384700f1c27
minyang3194 <50267545+minyang3194@users.noreply.github.com>
2025-06-01 02:13:12 +0800

    Update diy-8820s.sh

--- diy-8820s.sh
+++ diy-8820s.sh
@@ -17,7 +17,7 @@
 # rm -rf feeds/luci/applications/luci-app-passwall
 
 # Modify Default IP
-sed -i 's/192.168.1.1/192.168.3.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.99/g' package/base-files/files/bin/config_generate
 
 # Modify default hostname
 # sed -i 's/ImmortalWrt/Railgun/g' package/base-files/files/bin/config_generate
